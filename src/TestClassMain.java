51e36fb636b1119dd17caf13888c87682417282c
irina1 <irina.dedja@sisal.al>
2020-05-20 17:04:46 +0200

    Facebook login test with selenium chrome driver v.81

--- src/TestClassMain.java
+++ src/TestClassMain.java
@@ -1,5 +1,22 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class TestClassMain {
     public static void main(String[] args) {
-        System.out.println("My first console app on GitHub!");
+        //System.out.println("My first console app on GitHub!");
+        System.setProperty("webdriver.chrome.driver","libs\\chromedriver.exe");
+        WebDriver driver= new ChromeDriver();
+        String baseUrl="https://www.facebook.com/";
+        driver.get(baseUrl);
+        WebElement login=driver.findElement(By.id("email"));
+        login.sendKeys("User");
+        login.click();
+        WebElement pass=driver.findElement(By.id("pass"));
+        pass.sendKeys("12345678!.");
+        WebElement accedi=driver.findElement(By.id("u_0_b"));
+        accedi.click();
+        driver.close();
     }
 }
